362726d59cdd3ebf40b63eedc1091821952d80ff
mrbeeeard <99538152+mrbeeeard@users.noreply.github.com>
2022-02-13 15:54:12 +0100

    add terms for rewards

--- src/TokenPage/Rewards.js
+++ src/TokenPage/Rewards.js
@@ -27,8 +27,44 @@ export default function Rewards() {
       <Container>
         <Paper className={classes.paperContainer}>
           <Typography variant="h5" className={classes.title}>
-            Rewards
+            PANIC Rewards
           </Typography>
+          
+          <section>
+            <Typography variant="h6" className={classes.title}>
+              For Farmers:
+            </Typography>
+              <ul>
+                <li>
+                  PANIC rewards are subject to a 2 year vesting period, but can also be claimed early at a 50% penalty.
+                </li>
+                <li>
+                  Exiting before the end of the vesting period always incurs a 50% penalty no matter how early or late you choose to exit.
+                </li>
+                <li>
+                  The 50% penalty is distributed continuously to PANIC lockers rewarding long-term holders.
+                </li>
+              </ul>
+            </section>
+
+
+            <section>
+            <Typography variant="h6" className={classes.title}>
+              For PANIC lockers:
+            </Typography>
+              <ul>
+                <li>
+                  Lock dates are grouped by the week. Any lock between Monday to Sunday is grouped in the same week group, and will release at the same time 2 years later.
+                </li>
+                <li>
+                  Locked PANIC is subject to a mandatory 2 years lock, and cannot be unlocked early.
+                </li>
+                <li>
+                  PANIC rewars from locking PANIC can be claimed anytime with no penalty.
+                </li>
+              </ul>
+            </section>
+
         </Paper>
       </Container>
     </div>
