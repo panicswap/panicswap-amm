740dcb62d36ebb475319fdf501af10119b000500
lup1 <fcklupi@protonmail.com>
2022-02-25 15:38:40 +0100

    change swap design (again, lmao) to be more compatible with smaller screens
    maxValue could use a rounding value to ToFixed(7) or so, had errors I could not figure out so left the whole balance there

--- src/CoinSwapper/CoinAmountInterface.js
+++ src/CoinSwapper/CoinAmountInterface.js
@@ -9,83 +9,95 @@ const useStyles = makeStyles((theme) => ({
     paddingRight: theme.spacing(1),
     fontWeight: "bold",
   },
+  percentages: {
+    marginLeft: "10px",
+  },
 }));
-
-
 
 export default function CoinAmountInterface(props) {
   // Requires all props required by <CoinField>, plus the following:
   //    maxValue - the max value that an input can have (eg. wallet balance for a specific token)
-  // 
+  //
   //    note: all other props are forwarded to the child component <CoinField>
-  // 
-
+  //
 
   const classes = useStyles();
 
   const { value, maxValue, onChange, symbol, maxWeiValue, decimals } = props;
 
-
   const updateAmount = (e, percentage = 100) => {
     e.preventDefault();
     console.log("starting");
     let amount;
-    
-    if(percentage && maxValue && symbol === "FTM") {
-      if(maxValue < 1){
+
+    if (percentage && maxValue && symbol === "FTM") {
+      if (maxValue < 1) {
         amount = 0;
-      }
-      else {
-        const weiAm = maxWeiValue.sub(1e18).mul( percentage ).div(100);
+      } else {
+        const weiAm = maxWeiValue.sub(1e18).mul(percentage).div(100);
         amount = ethers.utils.formatUnits(String(weiAm), decimals);
       }
-    } else if(maxValue) {
-      console.log("decimals ==",decimals);
-      const weiAm = maxWeiValue.mul( percentage ).div(100);
+    } else if (maxValue) {
+      console.log("decimals ==", decimals);
+      const weiAm = maxWeiValue.mul(percentage).div(100);
       amount = ethers.utils.formatUnits(String(weiAm), decimals);
     } else {
       amount = 0;
     }
     console.log("MAX CLICKED", amount);
-    const mockEvent = { target: {value: String(amount)} }
-    onChange(mockEvent)
-  }
-
+    const mockEvent = { target: { value: String(amount) } };
+    onChange(mockEvent);
+  };
 
   return (
     <>
       {/* Controls */}
       <div className={classes.controls}>
-        <Grid
-            container
-            direction="row"
-            justifyContent="flex-end"
-            spacing={2}
-        >
-            <Grid item>
-            <Link href="#" onClick={(e) => {updateAmount(e, 25)}}>
+        <Grid container direction="row" justifyContent="flex-end">
+          <Grid item className={classes.percentages}>
+            <Link
+              href="#"
+              onClick={(e) => {
+                updateAmount(e, 25);
+              }}
+            >
               25%
             </Link>
           </Grid>
-          <Grid item>
-            <Link href="#" onClick={(e) => {updateAmount(e, 50)}}>
+          <Grid item className={classes.percentages}>
+            <Link
+              href="#"
+              onClick={(e) => {
+                updateAmount(e, 50);
+              }}
+            >
               50%
             </Link>
           </Grid>
-          <Grid item>
-            <Link href="#" onClick={(e) => {updateAmount(e, 75)}}>
+          <Grid item className={classes.percentages}>
+            <Link
+              href="#"
+              onClick={(e) => {
+                updateAmount(e, 75);
+              }}
+            >
               75%
             </Link>
           </Grid>
-          <Grid item>
-            <Link href="#" onClick={(e) => {updateAmount(e, 100)}}>
+          <Grid item className={classes.percentages}>
+            <Link
+              href="#"
+              onClick={(e) => {
+                updateAmount(e, 100);
+              }}
+            >
               100%
             </Link>
           </Grid>
         </Grid>
       </div>
       {/* CoinField */}
-      <CoinField {...props}  />
+      <CoinField {...props} />
     </>
   );
 }
