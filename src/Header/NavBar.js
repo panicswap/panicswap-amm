eee2a361c1dd07e005724838364f3a23a21dddcd
lup1 <fcklupi@protonmail.com>
2022-03-22 01:32:14 +0100

    WIP: center bottom menu
    todo resize icons

--- src/Header/NavBar.js
+++ src/Header/NavBar.js
@@ -3,12 +3,12 @@ import { MenuItems } from "./MenuItems";
 
 const NavBar = () => {
   return (
-    <nav className="grid grid-cols-6 gap-1 max-w-md mx-auto mt-2 md:mt-0">
+    <nav className="flex flex-none gap-1 max-w-md mx-auto mt-2 md:mt-0">
       {MenuItems.map((item, index) => (
         <NavLink
           key={index}
           className="text-sm p-2 px-2 rounded-xl text-center md:px-3 text-white bg-blue-400 hover:bg-blue-500 transition-colors  hover:no-underline hover:text-white"
-          to={item.title == "Docs" || item.title == "Gov" ? {pathname : item.url} : item.url}
+          to={item.url}
         >
           {item.title}
         </NavLink>
