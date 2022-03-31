c7d8864713028a13a96d0c6c92df74e57dbe9c77
AdamAlberty <alberty.adam@gmail.com>
2022-03-31 09:11:31 +0200

    Change staked panic amount styles

--- src/TokenPage/Stake.js
+++ src/TokenPage/Stake.js
@@ -12,7 +12,7 @@ import {
   TableHead,
   TableRow,
   TableBody,
-  TableCell
+  TableCell,
 } from "@material-ui/core";
 import { useSnackbar } from "notistack";
 import {
@@ -23,7 +23,7 @@ import {
   getSigner,
   getNetwork,
   getWeth,
-  getEpsStaking
+  getEpsStaking,
 } from "../ethereumFunctions";
 import LoadingButton from "../Components/LoadingButton";
 import COINS from "../constants/coins";
@@ -38,8 +38,7 @@ const styles = (theme) => ({
   title: {
     marginBottom: theme.spacing(1),
   },
-  balance: {
-  },
+  balance: {},
   smallTokenIcon: {
     width: "20px",
     marginLeft: "5px",
@@ -52,7 +51,7 @@ const styles = (theme) => ({
   },
   buttonContainer: {
     padding: theme.spacing(2),
-  }
+  },
 });
 
 const useStyles = makeStyles(styles);
@@ -61,7 +60,6 @@ export default function Stake() {
   const classes = useStyles();
   const { enqueueSnackbar } = useSnackbar();
 
-
   const [provider, setProvider] = React.useState(getProvider());
   const [signer, setSigner] = React.useState(getSigner(provider));
 
@@ -69,9 +67,13 @@ export default function Stake() {
   const [account, setAccount] = React.useState(undefined);
   const [chainId, setChainId] = React.useState(undefined);
   const [router, setRouter] = React.useState(undefined);
-  const [stakingEps, setStakingEps] = React.useState(getEpsStaking("0x536b88CC4Aa42450aaB021738bf22D63DDC7303e",signer));
+  const [stakingEps, setStakingEps] = React.useState(
+    getEpsStaking("0x536b88CC4Aa42450aaB021738bf22D63DDC7303e", signer)
+  );
   const [weth, setWeth] = React.useState(undefined);
-  const [panic, setPanic] = React.useState(getWeth("0xA882CeAC81B22FC2bEF8E1A82e823e3E9603310B",signer));
+  const [panic, setPanic] = React.useState(
+    getWeth("0xA882CeAC81B22FC2bEF8E1A82e823e3E9603310B", signer)
+  );
   const [factory, setFactory] = React.useState(undefined);
   const [vestedBalance, setVestedBalance] = React.useState(0);
   const [lockedBalance, setLockedBalance] = React.useState("0");
@@ -102,7 +104,6 @@ export default function Stake() {
   // Controls the loading button
   const [loading, setLoading] = React.useState(false);
 
-
   // These functions take an HTML event, pull the data out and puts it into a state variable.
   const handleChange = {
     field1: (e) => {
@@ -113,10 +114,8 @@ export default function Stake() {
     },
   };
 
-
   // This hook will run when the component first mounts, it can be useful to put logs to populate variables here
   useEffect(() => {
-
     getAccount().then((account) => {
       setAccount(account);
     });
@@ -128,13 +127,16 @@ export default function Stake() {
       });
       if (chains.networks.includes(chainId)) {
         setwrongNetworkOpen(false);
-        console.log('chainID: ', chainId);
+        console.log("chainID: ", chainId);
         // Get the router using the chainID
-        const router = await getRouter(chains.routerAddress.get(chainId), signer);
+        const router = await getRouter(
+          chains.routerAddress.get(chainId),
+          signer
+        );
         setRouter(router);
         // Get Weth address from router
         await router.weth().then((wethAddress) => {
-          console.log('Weth: ', wethAddress);
+          console.log("Weth: ", wethAddress);
           setWeth(getWeth(wethAddress, signer));
           // Set the value of the weth address in the default coins array
           const coins = COINS.get(chainId);
@@ -143,46 +145,61 @@ export default function Stake() {
         // Get the factory address from the router
         await router.factory().then((factory_address) => {
           setFactory(getFactory(factory_address, signer));
-        })
+        });
       } else {
-        console.log('Wrong network mate.');
+        console.log("Wrong network mate.");
         setwrongNetworkOpen(true);
       }
     }
 
-    Network()
-
+    Network();
   }, [provider, signer]);
 
-  useEffect( () => {
+  useEffect(() => {
     const updateStakingStats = async () => {
       const promises = [
-        stakingEps.unlockedBalance(account)
-            .then(unlockedBal => setUnlockedBalance(ethers.utils.formatUnits(unlockedBal))),
-        stakingEps.withdrawableBalance(account)
-            .then(({1: vestedBalance}) => setVestedBalance(ethers.utils.formatUnits(vestedBalance)*2)),
-        stakingEps.claimableRewards(account)
-            .then(({1: {0: panicEarnedFinal }}) => setPanicRewards(ethers.utils.formatUnits(panicEarnedFinal))),
-        stakingEps.lockedBalances(account)
-            .then(({0: panicLockedTotal}) => setLockedBalance(ethers.utils.formatUnits(panicLockedTotal))),
-        panic.balanceOf(account)
-            .then(bal => {
-              setPanicWeiBalance(bal);
-              setPanicBalance(ethers.utils.formatUnits(bal));
-            })
+        stakingEps
+          .unlockedBalance(account)
+          .then((unlockedBal) =>
+            setUnlockedBalance(ethers.utils.formatUnits(unlockedBal))
+          ),
+        stakingEps
+          .withdrawableBalance(account)
+          .then(({ 1: vestedBalance }) =>
+            setVestedBalance(ethers.utils.formatUnits(vestedBalance) * 2)
+          ),
+        stakingEps
+          .claimableRewards(account)
+          .then(({ 1: { 0: panicEarnedFinal } }) =>
+            setPanicRewards(ethers.utils.formatUnits(panicEarnedFinal))
+          ),
+        stakingEps
+          .lockedBalances(account)
+          .then(({ 0: panicLockedTotal }) =>
+            setLockedBalance(ethers.utils.formatUnits(panicLockedTotal))
+          ),
+        panic.balanceOf(account).then((bal) => {
+          setPanicWeiBalance(bal);
+          setPanicBalance(ethers.utils.formatUnits(bal));
+        }),
       ];
       await Promise.allSettled(promises);
-    }
+    };
     updateStakingStats();
-
   }, [account, panic, stakingEps]);
 
-  async function stakePan(bal, lockrnt){
+  async function stakePan(bal, lockrnt) {
     const amountIn = ethers.utils.parseUnits(bal, 18);
-    const allo = await panic.allowance(account,"0x536b88CC4Aa42450aaB021738bf22D63DDC7303e");
-    if(Number(allo) < Number(amountIn)){
-      await panic.approve("0x536b88CC4Aa42450aaB021738bf22D63DDC7303e","99999999999999999999999999");
-      const delay = ms => new Promise(res => setTimeout(res, ms));
+    const allo = await panic.allowance(
+      account,
+      "0x536b88CC4Aa42450aaB021738bf22D63DDC7303e"
+    );
+    if (Number(allo) < Number(amountIn)) {
+      await panic.approve(
+        "0x536b88CC4Aa42450aaB021738bf22D63DDC7303e",
+        "99999999999999999999999999"
+      );
+      const delay = (ms) => new Promise((res) => setTimeout(res, ms));
       await delay(5000);
     }
     await stakingEps.stake(amountIn, lockrnt);
@@ -190,148 +207,135 @@ export default function Stake() {
 
   const hasBalance = {
     deposit: () => {
-      // @todo 
+      // @todo
       return false; // returns true if user has balance to deposit
     },
-  }
+  };
 
   return (
     <div>
-      <Container>
-        <Paper className={classes.paperContainer}>
+      <div className="dark:bg-slate-800 dark:text-white p-3 rounded-xl flex items-center">
+        <h4 className="font-display text-lg mr-auto">Staked PANIC</h4>
 
+        {/* @todo */}
+        {/* @todo --> need to iterate with myArray.map()  */}
+        {/* @todo */}
+
+        <div className="flex dark:bg-gray-900 p-1 rounded-md items-center ml-2">
+          <div className="p-1 dark:bg-gray-700 rounded-md dark:text-gray-400">
+            locked
+          </div>
+          <div className="ml-2">{Number(lockedBalance).toFixed(2)}</div>
+          <img
+            src="assets/token/PANIC.svg"
+            className={classes.smallTokenIcon}
+          />
+        </div>
+        <div className="flex dark:bg-gray-900 p-1 rounded-md items-center ml-2">
+          <div className="p-1 dark:bg-gray-700 rounded-md dark:text-gray-400">
+            vested
+          </div>
+          <div className="ml-2">{Number(vestedBalance).toFixed(2)}</div>
+          <img
+            src="assets/token/PANIC.svg"
+            className={classes.smallTokenIcon}
+          />
+        </div>
+        <div className="flex dark:bg-gray-900 p-1 rounded-md items-center ml-2">
+          <div className="p-1 dark:bg-gray-700 rounded-md dark:text-gray-400">
+            unlocked
+          </div>
+          <div className="ml-2">{Number(unlockedBalance).toFixed(2)}</div>
+          <img
+            src="assets/token/PANIC.svg"
+            className={classes.smallTokenIcon}
+          />
+        </div>
+      </div>
+
+      <h4>Stake Panic</h4>
+
+      <Typography variant="subtitle1">
+        Stake PANIC and earn platform fees in yvWFTM without lock-up.
+      </Typography>
+
+      <Grid container direction="row" justifyContent="center">
+        <Grid item xs={12}>
+          <CoinAmountInterface
+            activeField={true}
+            value={field1Value}
+            onClick={() => setDialog1Open(true)}
+            onChange={handleChange.field1}
+            symbol={tokenDetails.symbol}
+            userCanChoose={false}
+            maxValue={panicBalance}
+            decimals={tokenDetails.decimals}
+            maxWeiValue={panicWeiBalance}
+          />
+        </Grid>
+
+        <Grid item xs={12} className={classes.buttonContainer}>
+          <LoadingButton
+            loading={loading}
+            valid={true}
+            success={false}
+            fail={false}
+            onClick={() => {
+              stakePan(field1Value, false);
+            }}
+          >
+            Deposit
+          </LoadingButton>
+        </Grid>
+      </Grid>
+
+      {/* Stake & lock */}
+      <Paper className={classes.paperContainer}>
         <Typography variant="h5" className={classes.title}>
-            Staked PANIC
-          </Typography>
+          Stake & Lock Panic
+        </Typography>
 
-          <TableContainer>
-            <Table className={classes.table} size="small" aria-label="simple table">
+        <Typography variant="subtitle1">
+          <p>
+            Stake and lock PANIC, earn platform fees in yvWFTM + penalty fees in
+            unlocked PANIC.
+          </p>
 
-              <TableBody>
+          <p>
+            PANIC deposited and locked is subject to a 2 year lock. You will
+            continue to earn fees after the locks expire if you do not withdraw.
+          </p>
+        </Typography>
 
-                {/* @todo */}
-                {/* @todo --> need to iterate with myArray.map()  */}
-                {/* @todo */}
-
-                <TableRow>
-                  <TableCell>
-                    Staked & locked
-                  </TableCell>
-                  <TableCell>
-                    {Number(lockedBalance).toFixed(2)}
-                    <img src="assets/token/PANIC.svg" className={classes.smallTokenIcon}></img>
-                  </TableCell>
-                </TableRow>
-
-                <TableRow>
-                  <TableCell>
-                    Staked & vested
-                  </TableCell>
-                  <TableCell>
-                    {Number(vestedBalance).toFixed(2)}
-                    <img src="assets/token/PANIC.svg" className={classes.smallTokenIcon}></img>
-                  </TableCell>
-                </TableRow>
-
-                <TableRow>
-                  <TableCell>
-                    Staked & unlocked
-                  </TableCell>
-                  <TableCell>
-                    {Number(unlockedBalance).toFixed(2)}
-                    <img src="assets/token/PANIC.svg" className={classes.smallTokenIcon}></img>
-                  </TableCell>
-                </TableRow>
-
-              </TableBody >
-            </Table >
-          </TableContainer >
-        </Paper>
-        {/* Stake w/o locking */}
-        <Paper className={classes.paperContainer}>
-          <Typography variant="h5" className={classes.title}>
-            Stake Panic
-          </Typography>
-
-          <Typography variant="subtitle1">
-              Stake PANIC and earn platform fees in yvWFTM without lock-up.
-          </Typography>
-
-          <Grid container direction="row" justifyContent="center">
-            <Grid item xs={12}>
-              <CoinAmountInterface
-                activeField={true}
-                value={field1Value}
-                onClick={() => setDialog1Open(true)}
-                onChange={handleChange.field1}
-                symbol={tokenDetails.symbol}
-                userCanChoose={false}
-                maxValue={panicBalance}
-                decimals={tokenDetails.decimals}
-                maxWeiValue={panicWeiBalance}
-              />
-            </Grid>
-
-            <Grid item xs={12} className={classes.buttonContainer}>
-              <LoadingButton
-                loading={loading}
-                valid={true}
-                success={false}
-                fail={false}
-                onClick={() => {stakePan(field1Value,false)}}
-              >
-                Deposit
-              </LoadingButton>
-            </Grid>
+        <Grid container direction="row" justifyContent="center">
+          <Grid item xs={12}>
+            <CoinAmountInterface
+              activeField={true}
+              value={field2Value}
+              onClick={() => setDialog2Open(true)}
+              onChange={handleChange.field2}
+              symbol={tokenDetails.symbol}
+              userCanChoose={false}
+              decimals={tokenDetails.decimals}
+              maxValue={panicBalance}
+              maxWeiValue={panicWeiBalance}
+            />
           </Grid>
-        </Paper>
-
-
-        {/* Stake & lock */}
-        <Paper className={classes.paperContainer} >
-          <Typography variant="h5" className={classes.title}>
-            Stake & Lock Panic
-          </Typography>
-
-          <Typography variant="subtitle1">
-            <p>
-              Stake and lock PANIC, earn platform fees in yvWFTM + penalty fees in unlocked PANIC.
-            </p>
-
-            <p>
-              PANIC deposited and locked is subject to a 2 year lock. You will continue to earn fees after the locks expire if you do not withdraw.
-            </p>
-          </Typography>
-
-          <Grid container direction="row" justifyContent="center">
-            <Grid item xs={12}>
-              <CoinAmountInterface
-                activeField={true}
-                value={field2Value}
-                onClick={() => setDialog2Open(true)}
-                onChange={handleChange.field2}
-                symbol={tokenDetails.symbol}
-                userCanChoose={false}
-                decimals={tokenDetails.decimals}
-                maxValue={panicBalance}
-                maxWeiValue={panicWeiBalance}
-              />
-            </Grid>
-            <Grid item xs={12} className={classes.buttonContainer}>
-              <LoadingButton
-                loading={loading}
-                valid={true}
-                success={false}
-                fail={false}
-                onClick={() => {stakePan(field2Value,true)}}
-              >
-                Lock
-              </LoadingButton>
-            </Grid>
+          <Grid item xs={12} className={classes.buttonContainer}>
+            <LoadingButton
+              loading={loading}
+              valid={true}
+              success={false}
+              fail={false}
+              onClick={() => {
+                stakePan(field2Value, true);
+              }}
+            >
+              Lock
+            </LoadingButton>
           </Grid>
-        </Paper>
-
-      </Container>
+        </Grid>
+      </Paper>
     </div>
   );
 }
